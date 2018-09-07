4d6f9b95f391a5c15a4aad43945fb28721a0b7a8
zhongxuyang <xyzhong188@163.com>
2018-09-07 15:14:42 +0800

    bug fix

--- src/main/java/com.zxy/controller/MessageController.java
+++ src/main/java/com.zxy/controller/MessageController.java
@@ -9,14 +9,13 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 /**
  * @Description:
  * @Author: Yeliang
  * @Date: Create in 13:53 2018/9/7
  */
 @Controller
-@RequestMapping(value = "message")
+@RequestMapping(value = "/message")
 public class MessageController {
     public static final Logger log = LoggerFactory.getLogger(MessageController.class);
 
